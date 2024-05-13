c4e38bb356b9a125928b3e2175ae327bb9d9ef2c
adyajudha <adityayudhapratama127@outlook.com>
2024-05-14 00:22:31 +0700

    Add jQuery lib & Bootstrap

--- index.php
+++ index.php
@@ -1,43 +1,97 @@
 <?php
     session_start();
+    ob_start();
+
     include "library/config.php";
 
     if(empty($_SESSION['username'])or empty($_SESSION['password'])){
         echo "<p align='center'>Anda harus login terlebih dahulu</p>";
-        echo "<meta http-equiv='refresh' content='2;url=login.php'>";
+        echo "<meta http-equiv='refresh' content='0;url=login.php'>";
     } else {
         define('INDEX', true);
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bootstrap-4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="open-iconic-master/font/css/open-iconic-bootstrap.min.css">
 </head>
-<body>
-    <header>
-        Aplikasi Manajemen Pegawai
-    </header>
-    <div class="container">
-        <aside>
-            <ul class="menu">
-                <li><a href="?hal=dashboard" class="aktif">Dashboard</a></li>
-                <li><a href="?hal=pegawai">Data Pegawai</a></li>
-                <li><a href="?hal=jabatan">Data Jabatan</a></li>
-                <li><a href="logout.php">Keluar</a></li>
+<body class="h-100">
+    <nav class="navbar navbar-expand-sm navbar-dark sticky-top-bg-info bg-primary">
+        <a href="#" class="navbar-brand">Manajemen Pegawai</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#sidebar" aria-control="sidebar" aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <nav class="collapse navbar-collapse" id="sidebar">
+            <ul class="navbar-nav d-sm-none">
+                <li class="nav-items">
+                    <a href="?hal=dashboard" class="nav-link text-white">
+                        <i class="oi oi-dashboard"></i> Dashboard
+                    </a>
+                </li>
+                <li class="nav-items">
+                    <a href="?hal=pegawai" class="nav-link text-white">
+                        <i class="oi oi-person"></i> Data Pegawai
+                    </a>
+                </li>
+                <li class="nav-items">
+                    <a href="?hal=jabatan" class="nav-link text-white">
+                        <i class="oi oi-sort-descending"></i> Data Jabatan
+                    </a>
+                </li>
+                <li class="nav-items">
+                    <a href="logout.php" class="nav-link text-white">
+                        <i class="oi oi-account-logout"></i> Keluar
+                    </a>
+                </li>
             </ul>
-        </aside>
-        <section class="main">
-            <?php include "konten.php"; ?>
-            <!-- <h1>Selamat Datang di Aplikasi Manajemen Pegawai</h1>
-            <h3>Anda login sebagai administrator</h3> -->
-        </section>
+        </nav>
+    </nav>
+
+    <div class="container-fluid h-100">
+        <div class="row h-100">
+            <nav class="col-md-2 col-sm-3 bg-dark h-100 p-0 position-fixed d-none d-sm-block">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item bg-dark">
+                        <a href="?hal-dashboard" class="nav-link text-white">
+                            <i class="oi oi-dashboard"></i> Dashboard
+                        </a>
+                    </li>
+                    <li class="list-group-item bg-dark">
+                        <a href="?hal=pegawai" class="nav-link text-white">
+                            <i class="oi oi-person"></i> Data Pegawai
+                        </a>
+                    </li>
+                    <li class="list-group-item bg-dark">
+                        <a href="?hal=jabatan" class="nav-link text-white">
+                            <i class="oi oi-sort-descending"></i> Data Jabatan
+                        </a>
+                    </li>
+                    <li class="list-group-item bg-dark">
+                        <a href="logout.php" class="nav-link text-white">
+                            <i class="oi oi-account-logout"></i> Keluar
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="col-md-10 col-sm-9 offset-md-2 offset-sm-3 mb-3">
+                <section class="main">
+                    <!-- <?php include "konten.php"; ?> -->
+                </section>
+            </div>
+        </div>
     </div>
-    <footer>
-        Copyright &copy; Aditya Yudha Pratama
-    </footer>
+    <div class="bg-secondary fixed-bottom">
+        <p class="m-2 text-center text-light">
+            <b>Copyright &copy; Aditya Yudha Pratama</b>
+        </p>
+    </div>
+
+    <script src="js/jQuery-3.3.1.min.js"></script>
+    <script src="bootstrap-4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
 <?php 
--- login.php
+++ login.php
@@ -1,21 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Aplikasi</title>
-    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="bootstrap-4.6.2/dist/css/bootstrap.min.css">
+
 </head>
-<body class="body-login">
+<body class="h-100 bg-info d-flex align-items-center">
     <div class="container">
-        <section class="login-box">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 mx-auto bg-light p-4">
+                <h3 class="text-center text-info pb-3 mb-3 border-bottom">Login Aplikasi</h3>
+                <form method="post" action="ceklogin.php">
+                    <input type="text" placeholder="Username" id="username" name="username" class="form-control form-control-lg mb-3">
+                    <input type="password" placeholder="Password" id="password" name="password" class="form-control form-control-lg mb-3">
+                    <input type="submit" value="Login" class="btn btn-info btn-lg btn-block">
+                </form>
+            </div>
+        </div>
+        <!-- <section class="login-box">
             <h2>Login Aplikasi</h2>
             <form method="post" action="ceklogin.php">
                 <input type="text" placeholder="Username" id="username" name="username">
                 <input type="password" placeholder="Password" id="password" name="password">
                 <input type="submit" value="Login">
             </form>
-        </section>
+        </section> -->
     </div>
 </body>
 </html>
